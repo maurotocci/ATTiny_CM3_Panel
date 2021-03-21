187e3672ef1acebea2ee13927671adfb37723b60
Mauro Tocci <mauro.tocci@multiconproject.com>
2021-03-21 07:00:23 +0100

    Update 20210321

--- Power Safe ATTiny microcontroller/Source 12.9/flash_bootloader.sh
+++ Power Safe ATTiny microcontroller/Source 12.9/flash_bootloader.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
 sudo avrdude -Cavrdude.conf -v -pattiny88 -cusbtiny -e -Uefuse:w:0xfe:m -Uhfuse:w:0b11010111:m -Ulfuse:w:0xE2:m -Uflash:w:optiboot_attiny88_8000000L.hex:i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
